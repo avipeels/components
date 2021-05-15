54de31fe8bc315fe4a9a759736e8ea1a70047389
Avinash PSK <avinashpsk@gmail.com>
2021-05-15 20:41:45 +0530

    added story for Label

---
+++ stories/Label.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Label from '../packages/Label/src/Label';
+
+export default {
+  title: 'Components/Label',
+  component: Label,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Label {...args} />;
+
+export const PrimaryLabel = Template.bind({});
+PrimaryLabel.args = {
+  name:'Avi'
+};
