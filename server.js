25adc42109aa4dacec0a8d554ea95d20877d93fc
Avinash <avinashpsk@gmail.com>
2021-06-05 22:29:51 +0530

    edited server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, "storybook-static", "index.html"));
 });
 
 app.listen(PORT, HOST);
