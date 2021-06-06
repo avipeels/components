6a7e08baf0c7d2f9f57aab1c631633c152c8a7de
Avinash <avinashpsk@gmail.com>
2021-06-06 13:39:06 +0530

    express.static

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const serveStatic = require('serve-static');
 const path = require('path');
-const morgan = require('morgan');
+
 // Constants
 const PORT = 8080;
 
 // App
 const app = express();
-app.use(morgan('dev'));
 
-const serve = serveStatic(path.join(__dirname, 'componentz'), { index: ['index.html'] });
-app.use('/', serve);
+// const serve = serveStatic(path.join(__dirname, 'componentz'), { index: ['index.html'] });
+// app.use('/', serve);
+app.use('/', express.static(path.join(__dirname, 'componentz')));
 
 app.listen(PORT);
 console.log(`Running`);
